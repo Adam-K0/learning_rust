87f489ef097f1b552959b8e00ad02f564ca37ed1
Adam Khattak <188712164+Adam-K0@users.noreply.github.com>
2025-12-18 12:22:55 -0600

    Added random_num, comparison, and loop to chapter 2

--- Guessing_Game/src/main.rs
+++ Guessing_Game/src/main.rs
@@ -1,15 +1,32 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the number!");
+    let secret_num = rand::thread_rng().gen_range(1..5); // produces integer in range [1,4]
+    //println!("Secret number is {secret_num}"); // uncomment for debugging
 
-    println!("Please input your guess.");
+    println!("  Guess the number!");
+    println!("---------------------");
+    loop{
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: i32 =  guess.trim().parse().expect("Please type a number");
 
-    println!("You guessed: {guess} {}, and {}", 5,7);
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("Spot on!");
+                break;
+            }
+        }
+    }
 }

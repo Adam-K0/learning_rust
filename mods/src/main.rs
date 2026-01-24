56135bcc34ec855c37be97f0d17bbaf8b01656ad
Adam Khattak <188712164+Adam-K0@users.noreply.github.com>
2026-01-24 16:45:35 -0600

    more refactoring

--- mods/src/main.rs
+++ mods/src/main.rs
@@ -4,11 +4,10 @@ mod submarine;
 
 
 fn main() {
-    use aerial_vehicle::airplane::Airplane;
-    use aerial_vehicle::airplane::UseCase;
+    use aerial_vehicle::airplane;
 
-    let x15 = Airplane{name : String::from("NASA X-15"),
-    usecase: UseCase::Other,
+    let x15 = airplane::Airplane{name : String::from("NASA X-15"),
+    usecase: airplane::UseCase::Other,
     max_passengers : 1,
     max_altitude_meters: 100000};
 
@@ -17,7 +16,7 @@ fn main() {
     x15.land();
 
     use land_vehicle::car;
-    let mut dreamCar = car::Car{
+    let mut dream_car = car::Car{
         num_wheels : 4,
         max_passengers : 2,
         top_speed_kph : 345,
@@ -25,6 +24,6 @@ fn main() {
         year: 2014
     };
     
-    dreamCar.year = 2013;
-    println!("The {} Porsche 918 Spyder can get up to {} km per hour!", dreamCar.year, dreamCar.top_speed_kph);
+    dream_car.year = 2013;
+    println!("The {} Porsche 918 Spyder can get up to {} km per hour!", dream_car.year, dream_car.top_speed_kph);
 }

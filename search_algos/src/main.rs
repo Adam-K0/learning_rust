0fd9122ae9522f2f70b4691ad62d025ffa77beec
Adam Khattak <188712164+Adam-K0@users.noreply.github.com>
2026-01-31 12:37:44 -0600

    built sequential search

--- search_algos/src/main.rs
+++ search_algos/src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
-    fn sequential(){
+    fn sequential(list: &[i32], target: i32) -> Option<&i32>{
+        for i in list{
+            if *i == target{
+               return Some(i);
+            } 
+        }
+        None
     }
 
     fn progressive_guessing(){
@@ -7,5 +13,15 @@ fn main() {
 
     fn rand_guess(){
     }
+    
+    // testing
+    let nums = vec![5,12,55,19];
+    let target = 55;
 
+    if let Some(refr) = sequential(&nums, target){
+        println!("found val {refr}"); // println will deref refr to print its val 
+    }
+    else{
+        println!("didn't find the val");
+    }
 }

9190a86a834e10ea4c09b5e447e1734a3baaab47
Adam Khattak <188712164+Adam-K0@users.noreply.github.com>
2026-05-02 16:42:57 -0500

    +mat mul & 2 tests

--- lin_alg/src/lib.rs
+++ lin_alg/src/lib.rs
@@ -12,11 +12,27 @@ pub fn transpose(mat: &Vec<Vec<usize>> ) -> Vec<Vec<usize>> {
 	
 	transpose	
 }
+
 pub fn scale_row(mat: & mut Vec<Vec<isize>>, row: usize, scale: isize) -> Vec<Vec<isize>>{
 	for entry in 0..mat[row].len() {
 		mat[row][entry] = mat[row][entry] * scale;
 	}
 	mat.to_vec()
+}
+
+pub fn mat_mul(mat1: & Vec<Vec<isize>>, mat2: & Vec<Vec<isize>>) -> Vec<Vec<isize>> {
+	let mut result = vec![vec![0; mat2[0].len()]; mat1.len()];
+
+	for row in 0..mat1.len() {
+		for col in 0..mat2[0].len(){
+			let mut sum  = 0;
+			for entry in 0..mat2.len(){
+				sum += mat1[row][entry] * mat2[entry][col];
+			}
+			result[row][col] = sum; 
+		}
+	}
+	result.to_vec()
 }
 
 #[cfg(test)]
@@ -28,23 +44,47 @@ mod tests {
 		let mat = vec![vec![1,2,3], vec![4,5,6], vec![7,8,9]];
         let result = transpose(&mat);
 		let correct = vec![vec![1,4,7], vec![2,5,8], vec![3,6,9]];
+
         assert_eq!(result, correct);
     }
+
 	#[test]
 	fn transpose_3x4(){
 		let mat = vec![vec![1,2,3,4], vec![5,6,7,8], vec![9,10,11,12]];
 		let result = transpose(&mat);
 		let correct = vec![vec![1,5,9], vec![2,6,10], vec![3,7,11], vec![4,8,12]];
+
 		assert_eq!(result, correct);
 	}
+
 	#[test]
 	fn scale_2x2(){
 		let mut mat = vec![vec![44,100], vec![0, -44]];
 		let result = scale_row(& mut mat, 1, 2);
 		let correct = vec![vec![44,100], vec![0, -88]];
 		let incorrect = vec![vec![44,100], vec![0, 88]];
+
 		assert_eq!(result, correct);
 		assert_ne!(result, incorrect);
 	}
-	
+
+	#[test]	
+	fn mat_mul_identity(){
+		let mat1 = vec![vec![1,0], vec![0, 1]];
+		let mat2 = vec![vec![44,100], vec![0, -44]];
+		let result = mat_mul(&mat1, &mat2);
+		let correct = mat2; 
+		
+		assert_eq!(result, correct);
+	}
+
+	#[test]
+	fn mat_mul_shared_dim(){
+		let mat1 = vec![vec![1,2], vec![-2,1]];
+		let mat2 = vec![vec![3,3], vec![1,1]];
+		let result = mat_mul(&mat1, &mat2);
+		let correct = vec![vec![5,5], vec![-5,-5]]; 
+
+		assert_eq!(result, correct);
+	}
 }
